8505ad89e250355efc0976d7a3568fd63214405f
Domenik Hofer <domenik.hofer@bluewin.ch>
2017-06-07 18:34:35 +0200

    DatePicker Implemented

--- js/scripts.js
+++ js/scripts.js
@@ -4,41 +4,30 @@ $(document).ready(function () {
     let notes = JSON.parse(localStorage.getItem("notes"));
     let param_id = getParams("id");
 
+    $(".input_datetime").datetimepicker({
+        minDate:0
+    });
 
     if (window.location.pathname.indexOf("index") > -1 && notes !== null) {
         loadNotes()
     }
-    ;
+
 
     if (param_id !== 0) {
         editNote(param_id);
     }
 
     $("#save").click(function () {
-        $(".input").removeClass("wrong");
-
-        $(".input_title").val().length < 3 ? $(".input_title").addClass("wrong") : "";
-        !/^[1-5]$/.test($(".input_importance").val()) ? $(".input_importance").addClass("wrong") : "";
-
-        !/^0*([1-9]|[12][0-9]|3[01])$/.test($(".input_due_D").val()) ? $(".input_due_D").addClass("wrong") : "";
-        !/^0*([1-9]|1[0-2])$/.test($(".input_due_M").val()) ? $(".input_due_M").addClass("wrong") : "";
-        $(".input_due_Y").val() < (new Date).getFullYear() ? $(".input_due_Y").addClass("wrong") : "";
-        !/^0*([0-9]|1[0-9]|2[0-4])$/.test($(".input_due_h").val()) ? $(".input_due_h").addClass("wrong") : "";
-        !/^0*([0-9]|[1-4][0-9]|5[0-9])$/.test($(".input_due_m").val()) ? $(".input_due_m").addClass("wrong") : "";
-
-        if ($(".wrong").length === 0) {
-            saveNote(param_id);
-        }
-
-
-    });
+        $(".input_importance").val(Math.min(Math.max(parseInt($(".input_importance").val()), 1), 5));
+        saveNote(param_id);
+ });
 
 
     $(".text").click(function () {
         $(this).toggleClass("long");
        $(this).find(".showMore").text(($(this).find(".showMore").text()==="▼"?"▲":"▼"));
 
-    })
+    });
 
     /*
      $(document).on("click", function (e) {
@@ -50,12 +39,12 @@ $(document).ready(function () {
 
     $(".input").on("click focus",function () {
         $(this).removeClass("wrong");
-    })
+    });
 
     $(document).on("click", ".edit", function () {
         let id = $(this).parent(".note").attr("id");
         window.location.replace("new.html?id=" + id);
-    })
+    });
 
 
     $(".sort_item").click(function () {
@@ -75,8 +64,7 @@ $(document).ready(function () {
     $(document).on("click", ".finished", function () {
         let id = $(this).parent().parent(".note").attr("id");
         checkNote(id);
-    })
-
+    });
 
     $(".style").change(function () {
         if ($(this).val() === "Greenday") {
@@ -100,7 +88,7 @@ $(document).ready(function () {
         filt_notes.forEach(note => {
             const note_content = comp_template({
                 note_id: note.id,
-                note_due: moment(note.due).fromNow(),
+                note_due: moment(note.due).isValid() ? moment(note.due, "YYYY/MM/DD HH:mm").fromNow() : note.due,
                 note_title: note.title,
                 note_importance: "!".repeat(note.importance),
                 note_checked: (note.checked === true ? "checked disabled" : ""),
@@ -108,12 +96,12 @@ $(document).ready(function () {
                 note_desc: note.desc
             });
             $("#notes_cont").append(note_content);
-        })
+        });
 
         $(".text").each(function () {
            if( $(this).prop("scrollHeight")>60){
 $(this).find(".showMore").show();
-           };
+           }
         });
 
     }
@@ -136,20 +124,16 @@ $(this).find(".showMore").show();
         $(".input_title").val(notes[id].title);
         $(".input_desc").val(notes[id].desc);
         $(".input_importance").val(notes[id].importance);
-        const due = notes[id].due.split(/[-:T]+/);
-        $(".input_due_Y").val(due[0]);
-        $(".input_due_M").val(due[1]);
-        $(".input_due_D").val(due[2]);
-        $(".input_due_h").val(due[3]);
-        $(".input_due_m").val(due[4]);
+        $(".input_datetime").val(notes[id].due);
+
     }
 
     function saveNote(param_id) {
         const notes_temp = [];
-        const title = $(".input_title").val();
-        const desc = $(".input_desc").val();
+        const title = $(".input_title").val()||"No Title";
+        const desc = $(".input_desc").val()|| "No Description";
         const importance = $(".input_importance").val();
-        const due = $(".input_due_Y").val() + "-" + pad($(".input_due_M").val()) + "-" + pad($(".input_due_D").val()) + "T" + pad($(".input_due_h").val()) + ":" + pad($(".input_due_m").val());
+        const due = $(".input_datetime").val() || "Whenever you want";
         const checked = false;
         const checked_on = false;
         const created = moment();
@@ -192,11 +176,6 @@ $(this).find(".showMore").show();
         return (results !== null ? results[1] : 0);
     }
 
-    function pad(n) {
-        const n2 = n.split("");
-        return (n < 10 && n2.length === 1) ? ("0" + n) : n;
-    }
+    });
 
-});
-
-// single page? / irgendwann /buuble click evnet? / header& Footer / better date input - array? datepicker?
+// single page?  /buuble click evnet? / header& Footer
